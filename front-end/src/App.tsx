b07dcf76721fca750f35c2efe323ad8850395535
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com.>
2026-05-03 00:38:18 +0330

    add vite-plugin-pages

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,18 @@
 import PWABadge from "./PWABadge.tsx";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/ui/errorFallback.tsx";
+import { useRoutes } from "react-router-dom";
+import routes from "virtual:generated-pages-react";
 function App() {
+  function AppRoutes() {
+    const element = useRoutes(routes);
+    return element;
+  }
   return (
     <>
-      <ErrorBoundary FallbackComponent={ErrorFallback}></ErrorBoundary>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <AppRoutes />
+      </ErrorBoundary>
       <PWABadge />
     </>
   );
--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -2,12 +2,17 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import Pages from 'vite-plugin-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+     Pages({
+      dirs: 'src/pages',
+      extensions: ['tsx', 'jsx', 'ts', 'js'],
+      importMode: 'async',
+    }),
     VitePWA({
       registerType: "prompt",
       injectRegister: false,
